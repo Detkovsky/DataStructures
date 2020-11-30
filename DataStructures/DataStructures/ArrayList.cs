59f429793c4c1377972fdaf74f222e1827a6f809
Detkovsky <alex1detkov@gmail.com>
2020-11-30 15:48:36 +0300

    no test prjct

---
+++ DataStructures/DataStructures/ArrayList.cs
@@ -0,0 +1,328 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class ArrayList
+    {
+        public int[] _array;
+        
+        public int Length { get; private set; }
+
+        public ArrayList()
+        {
+            _array = new int[5];
+            Length = 0;
+        }
+         
+        public ArrayList(int value)
+        {
+            _array = new int[5];
+            _array[0] = value;
+            Length = 1;
+        }
+        
+        public ArrayList(int[] array)
+        {
+            _array = new int[(int)(array.Length * 1.33)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                _array[i] = array[i];
+            }
+            Length = array.Length;
+        }
+        
+        //добавить в конец
+        public void AddToTail(int value)
+        {
+            if (Length >= _array.Length)
+            {
+                RiseSize();
+            }
+            _array[Length] = value;
+            Length++;
+        }
+        
+        //увелить массив
+        public void RiseSize(int size = 1)
+        {
+            int newLength = Length;
+            while (newLength < Length + size)
+            {
+                newLength = (int)(1 + newLength * 1.33d);
+                int[] newArray = new int[newLength];
+                Array.Copy(_array, newArray, _array.Length);
+                _array = newArray;
+
+            }
+        }
+        
+        // добавление в начало
+        public void AddToHead(int value)
+        {
+            if (Length >= _array.Length)
+            {
+                RiseSize();
+            }
+            Move(_array.Length);
+            _array[0] = value;
+            Length++;
+        }
+        
+        private void Move(int quantity)
+        {
+            int[] newArray = new int[_array.Length];
+            Array.Copy(_array, 0, newArray, quantity, Length);
+            _array = newArray;
+        }
+
+        
+        // добавить по индексу
+        public void AddByIndex(int index, int value)
+        {
+            if (index > Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (Length >= _array.Length)
+            {
+                RiseSize();
+            }
+            Move(_array.Length);
+            _array[index] = value;
+            Length++;
+        }
+
+        
+        // удалить посл значение
+        public void DeleteLastValue()
+        {
+            if (Length >= _array.Length)
+            {
+                RiseSize();
+            }
+            Length = Length - 1;
+        }
+        
+        // удалить первый эл-т
+        public void DeleteFirstValue()
+        {
+            if (Length >= _array.Length)
+            {
+                RiseSize();
+            }
+            for (int i = 0; i < _array.Length - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+            Length = Length - 1;
+        }
+        
+        //удалить по инлексу
+        public void DeleteByIndex(int index)
+        {
+            if ((index >= 0) && (index < Length))
+            {
+                for (int i = index; i < Length - 1; i++)
+                {
+                    _array[i] = _array[i + 1];
+                }
+                Length--;
+            }
+        }
+        
+        // индекс по значению
+        public int TakeIndex(int value)
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                if (_array[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        
+        //изменить по индексу
+        public void ChangeByIndex(int value, int tmp)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (i == value - 1)
+                {
+                    _array[i] = tmp;
+                }
+            }
+            if (value > Length)
+            {
+                throw new IndexOutOfRangeException("");
+            }
+
+        }
+
+        //реверс
+        public void Reverse()
+        {
+            for (int i = 0; i < Length / 2; i++)
+            {
+                int a = _array[i];
+                _array[i] = _array[Length - 1 - i];
+                _array[Length - 1 - i] = a;
+            }
+        }
+
+        // поиск максимального
+        public int FindMaxElement()
+        {
+            int max = _array[0];
+            for (int i = 0; i < Length; i++)
+            {
+                if (_array[i] > max)
+                {
+                    max = _array[i];
+                }
+            }
+            return max;
+        }
+
+        //поиск минимального
+        public int FindMinElement()
+        {
+            int min = _array[0];
+            for (int i = 0; i < Length; i++)
+            {
+                if (_array[i] < min)
+                {
+                    min = _array[i];
+                }
+            }
+            return min;
+        }
+        
+        public int FindIndexOfMaxElement()
+        {
+            int indexMax = 0;
+            int max=_array[0]; 
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] > max)
+                {
+                    max = _array[i];
+                    indexMax = i;
+                }
+            }
+            return indexMax;
+        }
+
+        public int FindIndexOfMinElement()
+        {
+            int indexMin = 0;
+            int min = _array[0];
+            for (int i = 0; i < Length; i++)
+            {
+                if (_array[i] < min)
+                {
+                    indexMin = i;
+                }
+            }
+            return indexMin;
+        }
+
+        //сортировка по возрастанию
+        public void SortFromMinToMax()
+        {
+            for (int i = 1; i < Length; i++)
+            {
+                int j;
+                int x = _array[i];
+                for (j = i - 1; j >= 0; j--)
+                {
+                    if (_array[j] < x)
+                        break;
+
+                    _array[j + 1] = _array[j];
+                }
+                _array[j + 1] = x;
+            }
+        }
+        
+        //сортировка по убыванию
+        public void SortFromMaxToMin()
+        {
+            for (int i = 1; i < Length; i++)
+            {
+                int j;
+                int x = _array[i];
+                for (j = i - 1; j >= 0; j--)
+                {
+                    if (_array[j] > x)
+                        break;
+
+                    _array[j + 1] = _array[j];
+                }
+                _array[j + 1] = x;
+            }
+        }
+        
+        //удаление N элементов из конца
+        public void DeleteFromTail(int quantity)
+        {
+            Length = Length - quantity;
+        }
+        
+        //добавление массива в начало
+        public void AddToHead(int[] array)
+        {
+            if (Length + array.Length >= _array.Length)
+            {
+                RiseSize(array.Length);
+            }
+            Move(array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                _array[i] = array[i];
+            }
+            Length++;
+        }
+        
+        //добавление массива в конец
+        public void AddToTail(int[] array)
+        {
+            if (Length + array.Length >= _array.Length)
+            {
+                RiseSize(array.Length);
+            }
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                _array[Length + count] = array[i];
+                count++;
+            }
+            Length += array.Length;
+        }
+        
+        //добавление массива по индексу
+        public void AddRagneByIndex(int index, int[] array)
+        {
+            if (index > Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (Length + array.Length >= _array.Length)
+            {
+                RiseSize(array.Length);
+            }
+            Move(_array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                _array[i + index] = array[i];
+            }
+            Length += array.Length;
+        }
+    }
+
+}
+
+
+    
+
---
+++ DataStructures/DataStructures/LinkedList.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class LinkedList
+    {
+
+        public int Length { get; private set; }
+
+        private Node _root;
+
+        public LinkedList()
+        {
+            Length = 0;
+            _root = null;
+        }
+
+        public LinkedList(int[] array)
+        {
+            Length = array.Length;
+            if (Length != 0)
+            {
+                _root = new Node(array[0]);
+                Node crt = _root;
+                for (int i = 1; i < Length; i++)
+                {
+                    crt.Next = new Node(array[i]);
+                    crt = crt.Next;
+                }
+            }
+            else
+            {
+                _root = null;
+            }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                CheckIndexOutOfRangeException(index);
+                Node tmp = _root;
+                for (int i = 0; i < index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                return tmp.Value;
+            }
+            set
+            {
+                CheckIndexOutOfRangeException(index);
+                Node tmp = _root;
+                for (int i = 0; i < index; i++)
+                {
+                    tmp = tmp.Next;
+                }
+                tmp.Value = value;
+            }
+        }
+
+        public void AddToTail(int value)
+        {
+            Node crt = _root;
+            for (int i = 0; i < Length - 1; i++)
+            {
+                crt = crt.Next;
+            }
+
+            Node tmp = new Node(value);
+            if (Length != 0)
+            {
+                crt.Next = tmp;
+            }
+            else
+            {
+
+                _root = tmp;
+            }
+            Length++;
+        }
+
+        
+
+        public void AddToHead(int value)
+        {
+            Node tmp = new Node(value);
+            tmp.Next = _root;
+            _root = tmp;
+            Length++;
+        }
+
+        
+
+        public void AddByIndex(int value, int index)
+        {
+            CheckIndexOutOfRangeException(index);
+            if (index != 0)
+            {
+                Node crt = _root;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    crt = crt.Next;
+                }
+                Node tmp = new Node(value);
+                tmp.Next = crt.Next;
+                crt.Next = tmp;
+                Length++;
+            }
+            else
+            {
+                AddToHead(value);
+            }
+        }
+
+        
+
+        public void RemoveLastElement (int amount = 1)
+        {
+            if (Length > amount)
+            {
+                Length -= amount;
+            }
+            else
+            {
+                Length = 0;
+            }
+        }
+
+        public void DeleteFirstElement(int amount = 1)
+        {
+            if (Length > amount)
+            {
+                Node crt = _root;
+                for (int i = 0; i < amount - 1; i++)
+                {
+                    crt = crt.Next;
+                }
+                _root = crt.Next;
+                Length -= amount;
+            }
+            else
+            {
+                Length = 0;
+            }
+        }
+
+        public void DeleteByIndex(int index, int size = 1)
+        {
+            if (index < 0 || index >= Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index == 0)
+            {
+                DeleteFirstElement(size);
+            }
+            else
+            {
+                Node crt = _root;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    crt = crt.Next;
+                }
+
+                if (size < Length - index)
+                {
+                    Node tmp = crt;
+                    for (int i = 0; i < size; i++)
+                    {
+                        crt = crt.Next;
+                    }
+                    tmp.Next = crt.Next;
+                    Length -= size;
+                }
+                else
+                {
+                    Length = index;
+                }
+            }
+        }
+
+        public int ReturnIndex(int element)
+        {
+            Node crt = _root;
+            for (int i = 0; i < Length; i++)
+            {
+                if (crt.Value == element)
+                {
+                    return i;
+                }
+                crt = crt.Next;
+            }
+            throw new Exception("There is no this value index");
+        }
+
+        public void Reverse()
+        {
+            if (Length == 0)
+            {
+                return;
+            }
+            Node oldRoot = _root;
+            Node tmp;
+            while (oldRoot.Next != null)
+            {
+                tmp = oldRoot.Next;
+                oldRoot.Next = tmp.Next;
+                tmp.Next = _root;
+                _root = tmp;
+            }
+        }
+        
+        private void CheckEmpty()
+        {
+            if (Length == 0)
+            {
+                throw new Exception("Empty array");
+            }
+        }
+
+        public int FindMaxValue()
+        {
+            CheckEmpty();
+            Node crt = _root;
+            int max = crt.Value;
+            for (int i = 0; i < Length - 1; i++)
+            {
+                crt = crt.Next;
+                if (crt.Value > max)
+                {
+                    max = crt.Value;
+                }
+            }
+            return max;
+        }
+
+        public int FindMinValue()
+        {
+            CheckEmpty();
+            Node crt = _root;
+            int min = crt.Value;
+            for (int i = 0; i < Length - 1; i++)
+            {
+                crt = crt.Next;
+                if (crt.Value < min)
+                {
+                    min = crt.Value;
+                }
+            }
+            return min;
+        }
+
+        public int FindIndexOfMinValue()
+        {
+            CheckEmpty();
+            Node crt = _root;
+            int min = crt.Value;
+            int minIndex = 0;
+            for (int i = 0; i < Length - 1; i++)
+            {
+                crt = crt.Next;
+                if (crt.Value < min)
+                {
+                    min = crt.Value;
+                    minIndex = i + 1;
+                }
+            }
+            return minIndex;
+        }
+
+        public int FindIndexOfMaxValue()
+        {
+            
+            Node crt = _root;
+            int max = crt.Value;
+            int maxIndex = 0;
+            for (int i = 0; i < Length - 1; i++)
+            {
+                crt = crt.Next;
+                if (crt.Value > max)
+                {
+                    max = crt.Value;
+                    maxIndex = i + 1;
+                }
+            }
+            return maxIndex;
+        }
+
+        public void SortFromMinToMax()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (this[j] < this[j - 1])
+                    {
+                        int k = this[j];
+                        this[j] = this[j - 1];
+                        this[j - 1] = k;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void SortFromMaxToMin()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (this[j] > this[j - 1])
+                    {
+                        int k = this[j];
+                        this[j] = this[j - 1];
+                        this[j - 1] = k;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        
+        public void AddToTail(int[] addArray)
+        {
+            Node crt = _root;
+            for (int i = 0; i < Length - 1; i++)
+            {
+                crt = crt.Next;
+            }
+            LinkedList linkedList = new LinkedList(addArray);
+            if (Length != 0)
+            {
+                crt.Next = linkedList._root;
+            }
+            else
+            {
+
+                _root = linkedList._root;
+            }
+
+            Length += addArray.Length;
+        }
+
+        public void AddToHead(int[] addArray)
+        {
+            LinkedList linkedList = new LinkedList(addArray);
+            Node crt = linkedList._root;
+            for (int i = 0; i < addArray.Length - 1; i++)
+            {
+                crt = crt.Next;
+            }
+            if (addArray.Length != 0)
+            {
+                crt.Next = _root;
+                _root = linkedList._root;
+            }
+            Length += addArray.Length;
+        }
+
+        public void DeleteFirstValue(int value)
+        {
+            try
+            {
+                int index = ReturnIndex(value);
+                DeleteByIndex(index);
+            }
+            catch
+            {
+            }
+        }
+
+        public void DeleteAllValue(int value)
+        {
+            if (Length == 0)
+            {
+                return;
+            }
+            Node crt = _root;
+            Node tmp = crt;
+            while (tmp.Next != null)
+            {
+                if (crt.Value == value)
+                {
+                    if (_root.Value == crt.Value)
+                    {
+                        _root = crt.Next;
+                    }
+                    else
+                    {
+                        tmp.Next = crt.Next;
+                    }
+                    Length--;
+                }
+                else
+                {
+                    tmp = crt;
+                }
+                crt = crt.Next;
+            }
+
+        }
+
+        private void CheckIndexOutOfRangeException(int index)
+        {
+            if (index < 0 || index > Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+        }
+    }
+}
+
